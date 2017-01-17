a573939524627f8080b6196e9427684a85fe0afd
Mike Korcynski <mkorcy@gmail.com>
2017-01-17 16:21:08 -0500

    makes routes work with engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,44 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  mount TuftsModelsNg::Engine => "/", as: "tufts_models_engine"
+  mount UserImpersonate::Engine => "/impersonate", as: "impersonate_engine"
+  mount Blacklight::Engine => '/'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+    concern :searchable, Blacklight::Routes::Searchable.new
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+  end
+  resources :records, only: [:new]
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  unauthenticated do
+    root to: 'contribute#redirect'
+  end
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :contribute, as: 'contributions', :controller => :contribute, :only => [:index, :new, :create] do
+    collection do
+      get 'license'
+    end
+  end
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  devise_for :users
+  mount CurationConcerns::Engine, at: '/'
+  # resources :welcome, only: 'index'
+  # root 'catalog#index'
+  curation_concerns_collections
+  curation_concerns_basic_routes
+  curation_concerns_embargo_management
+  concern :exportable, Blacklight::Routes::Exportable.new
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  resources :bookmarks do
+    concerns :exportable
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+    collection do
+      delete 'clear'
+    end
+  end
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
